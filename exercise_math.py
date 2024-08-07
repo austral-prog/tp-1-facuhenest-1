8e35c0ad351ff154b3ca85c5ca8bc6e89bf2ba2d
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-08-07 12:26:04 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,12 @@
 def math():
     a = 57
     b = 7
+    
+    print (a + b) # 64
+    print (a - b) # 50
+    print (a * b) # 399
+    print ((a + b) / 2) # 32 
+    print (a // b) # 8
+    print (a % b) # 1
+    print (a / b) # 8,14...
+math()
